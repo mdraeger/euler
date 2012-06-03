6c689eb488572175d3805aa789ed8c16dd8004e9
Marco Draeger <draeger.marco@googlemail.com>
2012-06-03 16:06:49 +0200

    solved 4th problem

---
+++ src/main/scala/Euler4.scala
@@ -0,0 +1,20 @@
+package org.draegisoft.euler
+
+object Euler4 {
+  def euler4(digits: Int): Long = {
+    val upper = math.pow(10, digits).toInt
+    val lower = math.pow(10, digits -1).toInt
+    for (i <- (lower until upper).reverse)
+      for (j <- (lower until upper).reverse){
+        val candidate = i.toLong * j.toLong
+        if (isPalindrome(candidate))
+          return candidate
+      }
+    1L  
+  }
+
+  def isPalindrome(value: Long) = {
+    val s: String = value.toString
+    s == s.reverse
+  }
+}
